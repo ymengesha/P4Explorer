ebd752ac22a5a62ce028f95e51346c328d2945d5
Yonathan Negussie Mengesha <yonathan.neg@gmail.com>
2018-08-20 02:28:33 +0200

    Use tempfile module for temp dir

--- P4Explorer.py
+++ P4Explorer.py
@@ -4,6 +4,7 @@ import sublime_plugin
 import os
 import os.path
 import subprocess
+import tempfile
 
 __PLUGIN_NAME__='P4Explorer'
 
@@ -25,19 +26,12 @@ class P4Explorer(sublime_plugin.WindowCommand):
 			# TO-DO: consider doing sanity checks on the selected region
 			tmp_path = self.getTmpFilePath(perforce_path)
 
-			if(self.fetchPeforceFile(perforce_path, tmp_path)):
+			if self.fetchPeforceFile(perforce_path, tmp_path):
 				self.window.open_file(tmp_path)
 			# TO-DO: maybe consider syntax setting for the opened file
 
-
-	@staticmethod
-	def log(level, message):
-		log_message = "[{0}] {1}: {2}".format(__PLUGIN_NAME__, level, message)
-		print(log_message)
-		sublime.status_message(log_message)
-
 	def getTmpFilePath(self, perforcePath):
-		tmp_dir = os.environ['TEMP']
+		tmp_dir = tempfile.gettempdir()
 		# TO-DO: consider the path extension of the tmp file ?
 		tmp_file_name = perforcePath.lstrip('/')
 		return os.path.abspath(os.path.join(tmp_dir, __PLUGIN_NAME__, tmp_file_name))
@@ -53,6 +47,11 @@ class P4Explorer(sublime_plugin.WindowCommand):
 				return False
 		return True
 
+	@staticmethod
+	def log(level, message):
+		log_message = "[{0}] {1}: {2}".format(__PLUGIN_NAME__, level, message)
+		print(log_message)
+		sublime.status_message(log_message)
 
 
 
